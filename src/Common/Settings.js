f0bf77d1c863aa5e95c342eaaf7f4ef1904353a6
seungwoo jung <jsw9808@gmail.com>
2019-12-23 23:26:14 +0900

    [ADD] fontSettings

---
+++ src/Common/Settings.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+
+export const fontSettings = StyleSheet.create({
+    regular: {
+        fontFamily: "NotoSans-Medium"
+    },
+    bold: {
+        fontFamily: "NotoSans-Bold"
+    },
+    light: {
+        fontFamily: "NotoSans-Light",
+        fontWeight: "100"
+    },
+    spq_regular: {
+        fontFamily: "SpoqaHanSans-Regular"
+    },
+    spq_light: {
+        fontFamily: "SpoqaHanSans-Light",
+        fontWeight: "100"
+    }
+})
